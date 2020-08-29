7a1c73c7668451a3e3edb72133339014e083927d
gaurangi99 <imgaurangi.99@gmail.com>
2020-08-29 23:54:06 +0530

    updated 'insertKAfterEveryElement' method with simpler conditions

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -2,7 +2,7 @@ public class LinkedList {
 	Node head;
 
 	public static void main(String[] args) {
-		LinkedList l=new LinkedList();
+		LinkedList l = new LinkedList();
 		l.insert(3);
 		l.insert(4);
 		l.insert(5);
@@ -10,33 +10,29 @@ public class LinkedList {
 	}
 
 	public void insert(int k) {
-		if(head==null) {
-			Node newNode=new Node(k, head);
-		}
-		else {
-			Node cur=head;
-			while(cur!=null && cur.next!=null) {
-				cur=cur.next;
+		if (head == null) {
+			Node newNode = new Node(k, null);
+			head = newNode;
+		} else {
+			Node cur = head;
+			while (cur != null && cur.next != null) {
+				cur = cur.next;
 			}
-			Node newNode=new Node(k, head.next);
-			head.next=newNode;
+			Node newNode = new Node(k, head.next);
+			head.next = newNode;
 		}
 	}
-	
+
 	public Node insertKAfterEveryElement(int k) {
 		Node newNode;
-		if(head==null) {
-			newNode=new Node(k, null);
+		if (head == null) {
+			newNode = new Node(k, null);
 		}
-		Node cur=head;
-		while(cur.next!=null) {
-			newNode=new Node(k, cur.next);
-			cur.next=newNode;
-			cur=cur.next.next;
-		}
-		if(cur.next==null) {
-			newNode=new Node(k, null);
-			cur.next=newNode;
+		Node cur = head;
+		while (cur != null) {
+			newNode = new Node(k, cur.next);
+			cur.next = newNode;
+			cur = newNode.next;
 		}
 		return head;
 	}
@@ -60,39 +56,39 @@ public class LinkedList {
 	}
 
 	public static int countEven(Node head) {
-		int countEven=0;
-		Node cur=head;
-		while(cur!=null) {
-			if(cur.val%2==0) 
+		int countEven = 0;
+		Node cur = head;
+		while (cur != null) {
+			if (cur.val % 2 == 0)
 				countEven++;
-			cur=cur.next;
+			cur = cur.next;
 		}
 		return countEven;
 	}
-	
+
 	public static void printKthElement(Node head, int k) {
-		Node cur=head;
-		int count=1;
-		while(cur!=null) {
-			if(count%k==0)
+		Node cur = head;
+		int count = 1;
+		while (cur != null) {
+			if (count % k == 0)
 				System.out.printf("%d, ", cur.val);
-			cur=cur.next;
+			cur = cur.next;
 			count++;
 		}
 	}
-	
+
 	public static Node delete(Node head, int k) {
-		Node cur=head;
-		if(cur.val==k) {
-			head=head.next;
+		Node cur = head;
+		if (cur.val == k) {
+			head = head.next;
 		}
-		while(cur!=null && cur.next!=null) {
-			if(cur.next.val==k) {
-				cur.next=cur.next.next;
+		while (cur != null && cur.next != null) {
+			if (cur.next.val == k) {
+				cur.next = cur.next.next;
 			}
-			cur=cur.next;
+			cur = cur.next;
 		}
 		return head;
 	}
-	
+
 }
